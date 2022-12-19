42a3caa0fcbbbf408d9ee7654a73691bd120ce06
aksen510 <510cara@naver.com>
2022-12-19 15:28:18 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+// import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import notify from "./notify";
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
 }
+
+// class App extends Component {
+//   state = {
+//     SplitMe: null,
+//   };
+//   handleClick = async () => {
+//     const loadedModule = await import("./SplitMe");
+//     this.setState({
+//       SplitMe: loadedModule.default,
+//     });
+//   };
+
+//   render() {
+//     const { SplitMe } = this.state;
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p onClick={this.handleClick}>Hello React!</p>
+//           {SplitMe && <SplitMe />}
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
